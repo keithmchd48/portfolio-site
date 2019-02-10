833ae6e182d752a661850f4035337efb5fe78349
Keith M <keithmchd48@gmail.com>
2019-02-10 20:23:58 +0530

    Updates

---
+++ precache-manifest.1c30758f490e4b68f437e055bae32752.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "00d745313b6b1947ea0fccdb60e9d878",
+    "url": "/static/media/keithpro.00d74531.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b32fda549225aee60f64",
+    "url": "/static/js/main.b32fda54.chunk.js"
+  },
+  {
+    "revision": "f07b56dfa008c63cddb9",
+    "url": "/static/js/1.f07b56df.chunk.js"
+  },
+  {
+    "revision": "b32fda549225aee60f64",
+    "url": "/static/css/main.23dcd706.chunk.css"
+  },
+  {
+    "revision": "f07b56dfa008c63cddb9",
+    "url": "/static/css/1.c5bcef08.chunk.css"
+  },
+  {
+    "revision": "5bd02543dd4b5412831869d1653e3d12",
+    "url": "/index.html"
+  }
+];
